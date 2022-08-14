f120c80951709e83f02ef08c4c998623892a6784
GlebNalivaiko <110242308+GlebNalivaiko@users.noreply.github.com>
2022-08-15 00:26:15 +0300

    Upgraded my skills with arrays

--- src/TaskFiveArrayHomeworkFour.java
+++ src/TaskFiveArrayHomeworkFour.java
@@ -1,4 +1,4 @@
-public class MassiveTop {
+public class TaskFiveArrayHomeworkFour {
     public static void main(String[] args) {
         int[] arr = new int[10];
         for (int i = 0; i < arr.length; i++) {
